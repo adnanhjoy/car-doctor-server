30285a6c94730736372776e9079844f091b202d8
engadnanhossain <engr.adnanhossain@gmail.com>
2023-06-03 23:03:38 +0600

    added orders delete api

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ async function run() {
             }
             const cursor = ordersCollection.find(query);
             const orders = await cursor.toArray();
-            console.log(req.query)
             res.send(orders)
         })
 
@@ -58,6 +57,13 @@ async function run() {
             const order = req.body;
             const result = await ordersCollection.insertOne(order);
             res.send(result);
+        })
+
+        app.delete('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result)
         })
 
     } finally {
